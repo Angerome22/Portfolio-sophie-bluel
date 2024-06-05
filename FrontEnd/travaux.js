9eda808e36e756e7cb80e7be5390709318942325
Angerome22 <angeline.jerome12@gmail.com>
2024-06-05 14:39:24 +0200

    filtres fonctionnels

--- FrontEnd/travaux.js
+++ FrontEnd/travaux.js
@@ -5,39 +5,39 @@ const works = await reponse.json();
 //console.log(reponse);
 
 //Construction de la fonction qui va permettre de récupérer tous les travaux depuis l'API
-//function genererWorks(works) {
-// boucle qui va lister les projets
-for (let i = 0; i < works.length; i++) {
-  const figure = works[i];
-  //recuperation de l'élément du DOM qui accueillera les projets
-  const divGallery = document.querySelector(".gallery");
-  //création d'une balise figure pour accueillir chaque projet
-  const worksElement = document.createElement("figure");
+function genererWorks(works) {
+  // boucle qui va lister les projets
+  for (let i = 0; i < works.length; i++) {
+    const figure = works[i];
+    //recuperation de l'élément du DOM qui accueillera les projets
+    const divGallery = document.querySelector(".gallery");
+    //création d'une balise figure pour accueillir chaque projet
+    const worksElement = document.createElement("figure");
 
-  //creation des balises du projet
-  const imageElement = document.createElement("img");
-  imageElement.src = figure.imageUrl;
-  const titleElement = document.createElement("figcaption");
-  titleElement.innerText = figure.title;
-  const categoryIdElement = document.createElement("p");
-  categoryIdElement.innerText = figure.categoryId;
-  const categoryElement = document.createElement("p");
-  categoryElement.innerText = figure.category.name;
+    //creation des balises du projet
+    const imageElement = document.createElement("img");
+    imageElement.src = figure.imageUrl;
+    const titleElement = document.createElement("figcaption");
+    titleElement.innerText = figure.title;
+    const categoryIdElement = document.createElement("p");
+    categoryIdElement.innerText = figure.categoryId;
+    const categoryElement = document.createElement("p");
+    categoryElement.innerText = figure.category.name;
 
-  //on rattache la balise figure à la div gallery
-  divGallery.appendChild(worksElement);
-  //on rattache l'image et le title à figure(la balise)
+    //on rattache la balise figure à la div gallery
+    divGallery.appendChild(worksElement);
+    //on rattache l'image et le title à figure(la balise)
 
-  worksElement.appendChild(imageElement);
-  worksElement.appendChild(titleElement);
-  worksElement.appendChild(categoryIdElement);
-  worksElement.appendChild(categoryElement);
-  //on rattache la balise figure au body
-  //document.body.appendChild(figure);
+    worksElement.appendChild(imageElement);
+    worksElement.appendChild(titleElement);
+    worksElement.appendChild(categoryIdElement);
+    worksElement.appendChild(categoryElement);
+    //on rattache la balise figure au body
+    //document.body.appendChild(figure);
+  }
 }
-//}
-//premier affichage de la page
-//genererWorks(works);
+//appel de la fonction genererWorks
+genererWorks(works);
 // gestion des boutons filtres
 //recuperation de l'element du dom qui accueillera les filtres
 
@@ -70,26 +70,38 @@ boutonFiltrerTous.addEventListener("click", function () {
   const worksFiltres = works.filter(function (works) {
     return works.categoryId;
   });
-  console.log(worksFiltres);
+  // Effacement de l'écran et regénération de la page
+  document.querySelector(".gallery").innerHTML = "";
+  genererWorks(worksFiltres);
+  //console.log(worksFiltres);
 });
 // -----------------------ajout event litener sur le bouton Objets---------------------
 boutonFiltrerObjets.addEventListener("click", function () {
   const worksFiltres = works.filter(function (works) {
     return works.categoryId === 1;
   });
-  console.log(worksFiltres);
+  // Effacement de l'écran et regénération de la page
+  document.querySelector(".gallery").innerHTML = "";
+  genererWorks(worksFiltres);
+  //console.log(worksFiltres);
 });
 // -----------------------ajout event litener sur le bouton Appartements---------------------
 boutonFiltrerAppartements.addEventListener("click", function () {
   const worksFiltres = works.filter(function (works) {
     return works.categoryId === 2;
   });
-  console.log(worksFiltres);
+  // Effacement de l'écran et regénération de la page
+  document.querySelector(".gallery").innerHTML = "";
+  genererWorks(worksFiltres);
+  //console.log(worksFiltres);
 });
 // -----------------------ajout event litener sur le bouton Hotels et restaurants---------------------
 boutonFiltrerHotelsRestaurants.addEventListener("click", function () {
   const worksFiltres = works.filter(function (works) {
     return works.categoryId === 3;
   });
-  console.log(worksFiltres);
+  // Effacement de l'écran et regénération de la page
+  document.querySelector(".gallery").innerHTML = "";
+  genererWorks(worksFiltres);
+  //console.log(worksFiltres);
 });
