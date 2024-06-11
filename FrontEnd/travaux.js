11b1fcf5085889b3e9cc3a5a6a3ee110bf8a07b7
Angerome22 <angeline.jerome12@gmail.com>
2024-06-11 18:29:10 +0200

    filtres sur travaux

--- FrontEnd/travaux.js
+++ FrontEnd/travaux.js
@@ -23,15 +23,15 @@ function genererTravaux(listeTravaux) {
     const titleItem = document.createElement("figcaption");
     titleItem.innerText = projets.title;
     // creation de balise utile pour la récupération mais caché à l'affichage
-    //const categoryIdElement = document.createElement("p");
-    //categoryIdElement.innerText = figure.categoryId;
+    const categoryIdElement = document.createElement("p");
+    categoryIdElement.innerText = projets.categoryId;
 
     //on rattache l'objet figure à la div gallery
     divGallery.appendChild(projetItem);
     //on rattache les balises à chaque objet
     projetItem.appendChild(imageItem);
     projetItem.appendChild(titleItem);
-    //projetItem.appendChild(categoryIdElement);
+    projetItem.appendChild(categoryIdElement);
 
     //on rattache la balise figure au body
     //document.body.appendChild(figure);
@@ -41,64 +41,110 @@ function genererTravaux(listeTravaux) {
 //appel de la fonction genererWorks
 genererTravaux(listeTravaux);
 genererFiltres();
+//ajoutListenerFiltre();
 
 // ---------------gestion des boutons filtres--------------------------------
 // creation liste de noms
 function genererFiltres() {
   //création d'une liste des catégories avec l'id et le name
   const listeDesCategories = listeCategories.map((categorie) => categorie);
-  //verification de la liste
-  console.log(listeDesCategories);
-
+  //console.log(listeDesCategories);
   //recuperation de l'element du dom qui accueillera les filtres
   const divFiltreCategories = document.querySelector(".filtre-categories");
   //creation de la balise button "tous" en dehors de la boucle puisque hors catégorie
   const boutonFiltrerTous = document.createElement("button");
-  boutonFiltrerTous.setAttribute("class", "btn-filtre");
+  boutonFiltrerTous.setAttribute("class", "btn-tous");
   boutonFiltrerTous.textContent = "Tous";
   divFiltreCategories.appendChild(boutonFiltrerTous);
-
+  //boucle pour afficher le nom des catégories de l'api
   for (let i = 0; i < listeDesCategories.length; i++) {
     //console.log("test");
     const boutonFiltrer = document.createElement("button");
     //recup de l'id du bouton pour le futur event listener
     boutonFiltrer.dataset.id = listeDesCategories[i].id;
+    //console.log(boutonFiltrer.dataset.id); //affiche bien 1 2 et 3 dans la console
     boutonFiltrer.setAttribute("class", "btn-filtre");
     boutonFiltrer.innerText = listeDesCategories[i].name;
     divFiltreCategories.appendChild(boutonFiltrer);
   }
+}
+// -----------------------ajout event litener sur le bouton ---------------------
 
-  // -----------------------ajout event litener sur le bouton tous---------------------
-  boutonFiltrerTous.addEventListener("click", function () {
-    genererTravaux(listeTravaux);
-  });
-  // Effacement de l'écran et regénération de la page  A revoir
-  //document.querySelector(".gallery").innerHTML = "";
-  //genererworks(); //
-} /*
-// -----------------------ajout event litener sur les boutons filtres---------------------
-//on recupere toutes les balises buttons presentes dans la section filtres-categories
-//const boutonFiltrerCategorie = document.querySelector(
-//  ".filtres-categories button"
-// );
+const listeBoutons = document.querySelectorAll(".btn-filtre");
+for (let i = 0; i < listeBoutons.length; i++) {
+  const boutonActuel = listeBoutons[i];
+  console.log(boutonActuel);
+  boutonActuel.addEventListener("click", (event) => {
+    console.log(event.currentTarget);
+    const travauxFiltres = listeTravaux.filter(function (listeTravaux) {
+      //ici appel de la liste filtre pour la categorie recupere dans l'event
+    });
+    //Effacement de l'écran et regénération de la page
+    //document.querySelector(".gallery").innerHTML = "";
 
-// boutonFiltrerCategorie[i].addEventListener("click", function (event) {
-//  const id = event.target.dataset.id;
-//    return works.categoryId === 1;
-//  });
-// Effacement de l'écran et regénération de la page
-//   document.querySelector(".gallery").innerHTML = "";
-//  genererWorks(worksFiltres);
-//console.log(worksFiltres);
-// });
-//  }*/
-// -----------------------ajout event litener sur le bouton Objets---------------------
-/*boutonFiltrerObjets.addEventListener("click", function () {
-  const worksFiltres = works.filter(function (works) {
-    return works.categoryId === 1;
+    //genererTravaux(listeTravaux);
   });
-  // Effacement de l'écran et regénération de la page
+}
+/*document.querySelectorAll(".btn-filtre").forEach((button) => {
+  //console.log(button);
+  button.addEventListener("click", function (event) {
+    console.log(event.currentTarget);
+    const travauxFiltres = listeTravaux.filter(function (listeTravaux) {
+      return listeTravaux.categoryId;
+    });
+  });
+  //Effacement de l'écran et regénération de la page
   document.querySelector(".gallery").innerHTML = "";
-  genererWorks(worksFiltres);
-  //console.log(worksFiltres);
+  genererTravaux(listeTravaux);
+});
+
+const listeTravauxFiltres = listeTravaux.filter(function (listeTravaux) {
+  console.log(listeTravaux.categoryId === 1);
 });*/
+
+/*button.addEventListener("click", function () {
+  const travauxFiltres = listeTravaux.filter(function (listeTravaux) {
+  if (listeTravaux.categoryId === 1) {
+    return listeTravaux.categoryId;
+   }
+  });
+  //Effacement de l'écran et regénération de la page
+  document.querySelector(".gallery").innerHTML = "";
+  genererTravaux(listeTravaux);
+});*/
+
+//function onClickFiltre(event) {
+// console.log(event.currentTarget);
+//création d'une liste des catégories avec l'id et le name
+
+/*document.querySelectorAll("button").forEach((button) => {
+  button.addEventListener("click", function (event) {
+    const projetsFiltres = listeTravaux.filter(function (listeTravaux) {
+      console.log(projetsFiltres);
+    });
+    //Effacement de l'écran et regénération de la page
+    document.querySelector(".gallery").innerHTML = "";
+    genererProjets(projetsFiltres);
+  });
+});*/
+
+/*function ajoutListenerFiltre() {
+  const boutonFiltrer = document.querySelectorAll(".btn-filtre");
+  for (let i = 0; i < boutonFiltrer.length; i++) {
+    boutonFiltrer[i].addEventListener("click", function (event) {
+      const id = event.target.dataset.id;
+      console.log;
+      // genererTravaux(listeTravaux);
+    });*/
+/*}/*
+}*/
+// -----------------------ajout event litener sur le bouton Objets---------------------//
+//boutonFiltrerObjets.addEventListener("click", function () {
+//const projetsFiltres = projets.filter(function (projets) {
+// return projets.categoryId === 1;
+//});
+// Effacement de l'écran et regénération de la page
+//document.querySelector(".gallery").innerHTML = "";
+//genererProjets(projetsFiltres);
+//console.log(worksFiltres);
+//});
