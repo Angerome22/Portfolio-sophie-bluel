119865b13e57e4a8e7a57661017eea54136049b5
Angerome22 <angeline.jerome12@gmail.com>
2024-06-19 17:13:53 +0200

    test modale en cours

--- FrontEnd/travaux.js
+++ FrontEnd/travaux.js
@@ -5,12 +5,20 @@ const token = localStorage.getItem("authToken");
 if (token){
   //si le token est présent on utilise la fonction qui modifie l'interface d'admin d'index
   afficherInterfaceAdmin();
+
 }else{
   //sinon on execute l'interface classique
-  afficherInterfaceClassique();
+  //afficherInterfaceClassique();//
+  fetchData();
 }
 fetchData();
-})
+
+//ajout code de la modale
+
+ // Code pour afficher et fermer la modale
+ 
+
+
 function afficherInterfaceAdmin(){
   const divFiltreCategories = document.querySelector(".filtre-categories");
   if (divFiltreCategories){
@@ -32,8 +40,7 @@ function afficherInterfaceAdmin(){
     const linkIcon = document.createElement("a");
     const editIcon = document.createElement("i");
     const iconText = document.createElement("span");
-    linkIcon.href = "#modal1";
-    linkIcon.setAttribute("id", "openLinkModal")
+    linkIcon.href = "#modal1";    
     linkIcon.classList.add("js-modal");
     iconText.classList.add("modify")
     editIcon.classList.add("fa-regular","fa-pen-to-square","edit-icon");
@@ -45,8 +52,7 @@ function afficherInterfaceAdmin(){
     mesProjets.appendChild(linkIcon);  
   }
 }
-
-function afficherInterfaceClassique() {
+/*function afficherInterfaceClassique() {
   function afficherInterfaceNonAuthentifie() {
     const divFiltreCategories = document.querySelector(".filtre-categories");
     if (divFiltreCategories) {
@@ -58,9 +64,7 @@ function afficherInterfaceClassique() {
         loginButton.textContent = "Login";
     }
 }
-
-}
-
+}*/
 
 
 //console.log("test liaison");
@@ -79,7 +83,7 @@ ajouterListenerFiltres(listeTravaux);
 
 }
 
-//Construction de la fonction qui va permettre de récupérer tous les travaux depuis l'API
+//----------------------Construction de la fonction qui va permettre de récupérer tous les travaux depuis l'API---------------------------
 function genererTravaux(listeTravaux) {
   //recuperation de l'élément du DOM qui accueillera les projets
   const divGallery = document.querySelector(".gallery");
@@ -113,8 +117,6 @@ function genererTravaux(listeTravaux) {
     divGallery.appendChild(projetItem);    
   }
 }
-
-
 
 // ---------------gestion des boutons filtres--------------------------------
 
@@ -164,4 +166,45 @@ boutonFiltrerTous.addEventListener("click", () =>{
  }
  // appel de la fonction fetchData pour initialiser les travaux et les filtres
  fetchData();
- 
+
+//--------------------------fonction pour le login à la page de connexion---------------------------------------------------------
+
+ document.getElementById("loginForm").addEventListener("submit", async function(event) {
+  //permet de ne pas envoyer le formulaire tout de suite    
+          event.preventDefault();
+  //on declare les constantes qui vont récupérer les value de chaque input   
+          const email = document.getElementById("email").value;
+          const password = document.getElementById("password").value;
+          const errorMessage = document.getElementById("error-message");
+      
+          try {
+              //on fait une requete pour envoyer les données de connexion
+              const reponseLogin = await fetch("http://localhost:5678/api/users/login", {
+              //Création de la charge utile au format JSON
+                  method: "POST",
+                  headers: {"Content-Type": "application/json"},
+                  body: JSON.stringify({ email, password })
+              });         
+   
+              if (reponseLogin.ok) {
+                  const data = await reponseLogin.json();
+                  //stokage du token d'authentification pour pouvoir agir dans la page index
+                  const token = data.token
+                  localStorage.setItem("authToken", token);              
+                  //trouver le moyen de vérifier que le token est stoké avec un console.log
+                  // on desactive temporairement le window location et on peut voir dans la console le token
+                  //console.log(token); 
+                  // la connexion est réussie on retourne sur la page d'accueil en modification
+                  window.location.href = ("index.html");                                            
+              } else {
+                  // on complete la balise p prevu pour les erreurs
+                  const errorData = await reponseLogin.json();
+                  errorMessage.textContent = errorData.message || "Email ou mot de passe incorrect";
+              }
+           //on attrape aussi l'exeption qui peut etre une erreur de serveur   
+          } catch (error) {
+              errorMessage.textContent = "Erreur de connexion. Veuillez réessayer plus tard.";
+          }
+      });
+
+  
